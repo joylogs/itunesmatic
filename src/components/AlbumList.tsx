a0d6fe072c02a5d12fc7f97490b215163413ed33
Joy <joy.logs@gmail.com>
2024-01-30 22:50:46 +0530

    service api changes

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -3,20 +3,23 @@ import React from "react";
 import { FlatList, Text, View } from "react-native";
 
 
-const customData = require('../utils/mockdata.json');
+
 // console.log("=>", customData.innerArray);
 
-const AlbumList = (customData) => {
+
+const AlbumList = ({ customData: any }) => {
 
     return (
         <View>
-            <FlatList data = {
-                [
-                    {key: "value1"},
-                    {key: "value2"},
-                    {key: "value3"},
-                ]
-            } renderItem = {({item}) => <Text>{item.key}</Text>}
+            <FlatList
+                data={
+                    [
+                        { key: "customData" },
+                        { key: "customData" },
+                        { key: "customData" },
+                    ]
+                }
+                renderItem={({ item }) => <Text>{item.key}</Text>}
             >
             </FlatList>
         </View>
